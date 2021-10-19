c18815dca85c2edaa88ef521888f0b679ae72b35
Nam Anh Dang <namwarlockclash@gmail.com>
2021-10-19 21:14:35 +1100

    01knapsack

---
+++ dynamic_programming/01knapsack.py
@@ -0,0 +1,93 @@
+"""
+3 solutions to 0-1 knapsack problem, each optimises the space complexity
+from O(nw) to O(1w)
+x = capacity, v = values, w = weight, n = number of values/weightsß
+
+
+Basic 2D solution. 
+dp[i][j] = max value for capacity j from a subset of values 0,...,i
+
+Space complexity = O(nx)
+"""
+def O_nx(x, v, w):
+    n = len(v)
+    dp = [[0 for _ in range(x+1)] for __ in range(n)]
+
+    for i in range(n):
+        for j in range(x+1):
+            if i == 0:
+                # base case: either can or cannot fit the first item
+                dp[i][j] = v[0] if j >= w[0] else 0
+            else:
+                # the max value is at least as good as for the previous
+                # subset since given values 1,...,i, not including value i
+                # results in a subset of 1,...,i-1
+                dp[i][j] = dp[i-1][j]
+                # if can fit item i into capacity j, then another possible 
+                # candidate for max value is putting item i into the knapsack 
+                # and optimally filling the remaining capacity with previous
+                # i-1 items.
+                if w[i] <= j:
+                    dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]] + v[i])
+
+    return dp[n-1][x]
+
+"""
+In the basic solution, when looking at the row i, only need the values
+from row i-1. Keep track of the current and previous row at each iteration.
+
+Space complexity = O(2x)
+"""
+def O_2x(x, v, w):
+    n = len(v)
+    dp = [[0 for _ in range(x+1)] for __ in range(2)]
+    curr = dp[0]
+    prev = dp[1] # doesn't matter which order curr and prev initially
+
+    for i in range(n):
+        # swap the rows before iterating
+        curr, prev = prev, curr
+        for j in range(x+1):
+            if i == 0:
+                curr[j] = v[0] if j >= w[0] else 0
+            else:
+                curr[j] = prev[j]
+                if w[i] <= j:
+                    curr[j] = max(prev[j], prev[j-w[i]] + v[i])
+
+    return curr[x]
+
+"""
+When looking at entry dp[i][j], we only look at dp[i-1][k], where k <= j.
+So if we iterate the columns from right to left, the subproblems in a column
+that have not been reached still represent the previous row (i-1). So only 
+one row is needed.
+
+Space complexity = O(1x)
+"""
+def O_1x(x, v, w):
+    n = len(v)
+    dp = [0 for _ in range(x+1)]
+
+    for i in range(n):
+        for j in range(x,-1,-1):
+            if i == 0:
+                dp[j] = v[0] if j >= w[0] else 0
+            else:
+                if w[i] <= j:
+                    dp[j] = max(dp[j], dp[j-w[i]] + v[i])      
+
+    return dp[x]
+
+
+if __name__ == "__main__":
+    x = int(input("Enter capacity: "))
+    v = [int(x) for x in input("Enter values: ").split()]
+    w = [int(x) for x in input("Enter weights: ").split()]
+    if len(v) != len(w):
+        raise ValueError("Enter same number of values and weights")
+    print()
+    # Check that all 3 return the same solution
+    print(O_nx(x, v, w))
+    print(O_2x(x, v, w))
+    print(O_1x(x, v, w))
---
+++ dynamic_programming/bookshop01ks.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+typedef long long ll;
+
+const int MOD = 1e9 + 7;
+double EPS = 1e-9;
+int INF = 1000000005;
+long long INFF = 1000000000000000005ll;
+double PI = acos(-1);
+ 
+#define all(v) v.begin(), v.end() // all elements in a vector
+#define alla(arr, sz) arr, arr + sz // all elements in an array
+#define mine *min_element // min element 
+#define maxe *max_element // max element 
+
+#define nl '\n'; // newline
+
+// Print 1D/2D array/vector for debugging
+#define print1(a, n) for(int i=0;i<n;++i){cout<<a[i]<<' ';}cout<<nl;
+#define print2(a, m, n) for(int i=0;i<m;++i){for(int j=0;j<n;++j)\
+{cout<<a[i][j]<<' ';}cout<<nl;}
+
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+You are in a book shop which sells n different books. You know the price and 
+number of pages of each book.
+
+You have decided that the total price of your purchases will be at most x. 
+What is the maximum number of pages you can buy? You can buy each book at most 
+once.
+
+Input
+The first input line contains two integers n and x: the number of books and 
+the maximum total price.
+The next line contains n integers h1,h2,…,hn: the price of each book.
+The last line contains n integers s1,s2,…,sn: the number of pages of each book.
+
+Output
+Print one integer: the maximum number of pages.
+
+Constraints
+    1<=n<=1000
+    1<=x<=105
+    1<=h_i,s_i<=1000
+
+Example
+    Input:
+    4 10
+    4 8 5 3
+    5 12 8 1
+
+    Output:
+    13
+
+Explanation: You can buy books 1 and 3. Their price is 4+5=9 and the number of pages is 5+8=13.
+*/
+
+const int MAXM = 1000;
+const int MAXN = 1e5;
+ll dp[MAXN+1] = {0};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    /*
+    Standard 0-1 knapsack problem
+    Given some subset of 1,...,n, require that sum(h_i) <= x, such that
+    sum(s_i) is maximized.
+    */
+
+    int n, x; cin >> n >> x;
+    vector<int> h(n); // prices
+    vector<int> s(n); // number of pages
+    for (int i = 0; i < n; ++i) {
+        cin >> h[i];
+    }
+    for (int i = 0; i < n; ++i) {
+        cin >> s[i];
+    }
+    
+    fill(dp+1, dp+x+1, -1);
+
+    // no duplicates allowed => loop over books first
+    for (int i = 0; i < n; ++i) {
+        // 
+        for (int j = x-h[i]; j >= 0; --j) {
+            if (dp[j] != -1)
+                dp[j+h[i]] = max(dp[j+h[i]], dp[j]+s[i]);
+        }
+    }
+
+    for (int i = 1; i <= x; ++i) {
+        dp[i] = max(dp[i], dp[i-1]);
+    }
+    
+    cout << dp[x] << nl;
+    return 0;
+}
