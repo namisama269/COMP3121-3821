39871789092918e4b0ce97e6936525635e5e2edc
Nam Anh Dang <namwarlockclash@gmail.com>
2021-10-20 05:29:57 +1100

    dft

--- convolutions/sequences.py
+++ convolutions/sequences.py
@@ -1,7 +1,8 @@
 """
-Operation on 2 sequences A and B
+Operations on 2 sequences A and B
 A = [a0,a1,...,an], B = [b0,b1,...,bm]
 """
+
 def add(A, B):
     pass
 
@@ -9,7 +10,9 @@ def pmul(A, B):
     pass
 
 def dot(A, B):
-    pass
+    if len(A) != len(B):
+        raise ValueError("A and B must have same length")
+    return [A[i]*B[i] for i in range(len(A))]
 
 def concat(A, B):
     pass
@@ -26,7 +29,11 @@ def convolution(A, B):
             conv[i+k] += A[i]*B[k]
     return conv
 
+def padzero(A, newLen):
+    while len(A) < newLen:
+        A.append(0)
+
 if __name__ == "__main__":
-    A = [1,2,1]
+    A = [1,1]
     B = [1,2,1]
     print(convolution(A,B))
